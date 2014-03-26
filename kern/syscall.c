180b32570d434f329df82dfd54106350d1f54406
lzz <ldm5235@gmail.com>
2014-03-26 21:12:34 +0800

    Dispatches to the correct kernel function, passing the arguments. syscall()

--- kern/syscall.c
+++ kern/syscall.c
@@ -21,7 +21,12 @@ sys_cputs(const char *s, size_t len)
 	// Destroy the environment if not.
 
 	// LAB 3: Your code here.
-
+	/*int i=0;
+	for(;i<len;i+=4)
+	{
+		cons_putc((int *)s);
+		s+=4;
+	}*/	
 	// Print the string supplied by the user.
 	cprintf("%.*s", len, s);
 }
@@ -70,6 +75,26 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 	// Return any appropriate return value.
 	// LAB 3: Your code here.
 
-	panic("syscall not implemented");
+	int32_t r =0;
+	switch(syscallno)
+	{
+		case SYS_cputs:
+			sys_cputs((const char *)a1,(size_t)a2);
+			break;
+		case SYS_cgetc:
+			r = sys_cgetc();
+			break;
+		case SYS_getenvid:
+			r = sys_getenvid();
+			break;
+		case SYS_env_destroy:
+			r = sys_env_destroy((envid_t)a1);
+			break;
+		//case NSYSCALL:
+		default:
+			return -E_INVAL;
+	}	
+	return r;
+	//panic("syscall not implemented");
 }
 
